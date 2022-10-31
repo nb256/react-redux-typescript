153b151b756a4e48dcf1c58da84212d0181fa24d
Naim Buru <nbur4u@gmail.com>
2022-10-31 21:38:03 +0300

    feat: Add Loading function

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function Loading() {
+  return <Container>Loading tournaments ...</Container>;
+}
