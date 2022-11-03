9fc1e6808711204e26845d0ba7962af4c5e393e0
Naim Buru <nbur4u@gmail.com>
2022-11-03 15:08:37 +0300

    fixup! fix: Make retry on Error component to send search request

--- src/utils/AppReduxWrapper.tsx
+++ src/utils/AppReduxWrapper.tsx
@@ -1,9 +1,14 @@
 import React, { ReactNode } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../store';
 
 const AppReduxWrapper = ({ children }: { children: ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Router>
+      <Provider store={store}>{children}</Provider>
+    </Router>
+  );
 };
 
 export default AppReduxWrapper;
